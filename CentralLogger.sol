922af027aa0a931d29219ef8d74bbfa59170df3f
Ben Isak <benjamin.isak@gmx.de>
2018-03-09 13:42:02 +0100

    initial commit
    CentralLogger for multi-contract projects

---
+++ CentralLogger.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.4.17;
+
+contract CentralLogger {
+
+    // variables
+    address sender;
+    mapping (address => bool) public whitelist;
+    
+    //constructor
+    function CentralLogger() public {
+        sender = msg.sender;
+    }
+    
+    //events
+    event Registered(string message, address entry);
+    event UpdateSomething(string message);
+    
+    //modifiers
+    modifier onlyWhitelisted() { 
+        require(whitelist[msg.sender]);
+        _;
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == sender);
+        _;
+    }
+
+    //functions
+    function register(address contractAddr) public onlyOwner {
+        whitelist[contractAddr] = true;
+        Registered("A new entry was made to the whitelist.", contractAddr);
+    }
+
+    function logSomething () public onlyWhitelisted {
+        // your code..
+        // ...
+        UpdateSomething("Did some work..");
+    }
+}
